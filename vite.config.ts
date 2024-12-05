986a4c30589340ebba666864dda7b22bdf1bf652
Alexander Schumacher <6401757+story75@users.noreply.github.com>
2024-12-05 22:16:21 +0100

    create repo

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { sveltePhosphorOptimize } from 'phosphor-svelte/vite';
+
+export default defineConfig({
+	plugins: [sveltePhosphorOptimize(), sveltekit()]
+});
